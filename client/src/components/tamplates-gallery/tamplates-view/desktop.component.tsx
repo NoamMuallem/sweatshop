e1c0c3cd2e6db0e73c92e2618c0ade5b7397c0e1
NoamMuallem <noammu072@gmail.com>
2020-09-06 19:15:06 +0300

    implementing front end image compression

--- client/src/components/tamplates-gallery/tamplates-view/desktop.component.tsx
+++ client/src/components/tamplates-gallery/tamplates-view/desktop.component.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { TamplateI } from "../../../types/interfaces";
-import { Image, Button, Modal, Form, Alert } from "react-bootstrap";
+import { Button, Modal, Form, Alert } from "react-bootstrap";
 import TamplateMash from "./tamplate-view";
+import Compress from "compress.js";
 
 export interface DesktopViewProps {
   tamplates: { [key: string]: TamplateI };
@@ -22,14 +23,35 @@ const DesktopView: React.SFC<DesktopViewProps> = (props: DesktopViewProps) => {
   };
   const handleShow = () => setShow(true);
 
-  const onDrop = (files: File[]) => {
+  const onDrop = async (files: File[]) => {
     if (files[0]) {
-      const reader = new FileReader();
-      reader.addEventListener("load", () => {
-        setImgData(reader.result!);
-      });
-      reader.readAsDataURL(files[0]);
+      const compress = new Compress();
+
+      await compress
+        .compress([files[files.length - 1]], {
+          size: 0.09, // the max size in MB, defaults to 2MB
+          quality: 1, // the quality of the image, max is 1,
+          maxWidth: 250, // the max width of the output image, defaults to 1920px
+          maxHeight: 300, // the max height of the output image, defaults to 1920px
+          resize: true, // defaults to true, set false if you do not want to resize the image width and height
+        })
+        .then((data) => {
+          const file = Compress.convertBase64ToFile(data[0].data, data[0].ext);
+          const reader = new FileReader();
+          reader.addEventListener("load", () => {
+            setImgData(reader.result!);
+          });
+          reader.readAsDataURL(file);
+          console.log(file);
+        });
     }
+  };
+
+  const flushForm = () => {
+    set_id("");
+    setName("");
+    setImgData(undefined);
+    setError(null);
   };
 
   const onSubmit = () => {
@@ -38,6 +60,7 @@ const DesktopView: React.SFC<DesktopViewProps> = (props: DesktopViewProps) => {
     //to update tamplate
     if (newTamplate._id) {
       props.update!(newTamplate, imgData);
+      flushForm();
     } else {
       //new tamplate verify uniqu name and that ther is an image
       if (
@@ -51,6 +74,7 @@ const DesktopView: React.SFC<DesktopViewProps> = (props: DesktopViewProps) => {
       } else {
         //send new tamplate and close modal
         props.addTamplate!(newTamplate, imgData);
+        flushForm();
       }
     }
     handleClose();
@@ -77,7 +101,7 @@ const DesktopView: React.SFC<DesktopViewProps> = (props: DesktopViewProps) => {
           <Form.File.Input
             type="file"
             accept="image/png"
-            onChange={(e: any) => onDrop(e.target.files)}
+            onChange={async (e: any) => onDrop(e.target.files)}
           />
         </Form.File>
         {imgData ? (
@@ -101,7 +125,16 @@ const DesktopView: React.SFC<DesktopViewProps> = (props: DesktopViewProps) => {
   );
 
   return (
-    <div>
+    <div
+      style={{
+        margin: "auto",
+        padding: "0px",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
       {/*in case there are no tamplates*/}
       {Object.keys(props.tamplates).length === 0 ? (
         //for regular costumers, adding removing and updating templates is not an option
@@ -111,38 +144,67 @@ const DesktopView: React.SFC<DesktopViewProps> = (props: DesktopViewProps) => {
           </Button>
         ) : null
       ) : (
-        <div>
-          ההדפסים שלי
+        <div
+          style={{
+            margin: "auto",
+            padding: "0px",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <p style={{ margin: "auto" }}>ההדפסים שלי</p>
           {
             ////for regular costumers, adding removing and updating templates is not an option
             props.addTamplate ? (
-              <Button variant="primary" onClick={handleShow}>
+              <Button
+                style={{ margin: "auto" }}
+                variant="primary"
+                onClick={handleShow}
+              >
                 הוספת הדפס חדש
               </Button>
             ) : null
           }
-          {Object.values(props.tamplates).map((value) => {
-            return (
-              <div
-                key={value._id!}
-                onClick={
-                  //if admin give update option
-                  props.update
-                    ? () => {
-                        //set old tamplate data in state
-                        set_id(value._id!);
-                        setName(value.name);
-                        setImgData(value.imageBuffer!.toString());
-                        handleShow();
-                      }
-                    : undefined
-                }
-              >
-                <TamplateMash imageBuffer={value.imageBuffer!.toString()} />
-              </div>
-            );
-          })}
-          ;
+          <div
+            style={{
+              margin: "auto",
+              display: "flex",
+              flexDirection: "row",
+              width: "80%",
+              flexWrap: "wrap",
+            }}
+          >
+            {Object.values(props.tamplates).map((value) => {
+              return (
+                <div
+                  style={{
+                    width: "fit-content",
+                    height: "fit-content",
+                    margin: "0.5rem",
+                    borderRadius: "2.725rem",
+                  }}
+                  key={value._id!}
+                  onClick={
+                    //if admin give update option
+                    props.update
+                      ? () => {
+                          //set old tamplate data in state
+                          set_id(value._id!);
+                          setName(value.name);
+                          setImgData(value.imageBuffer!.toString());
+                          handleShow();
+                        }
+                      : undefined
+                  }
+                >
+                  <TamplateMash imageBuffer={value.imageBuffer!.toString()} />
+                </div>
+              );
+            })}
+            ;
+          </div>
         </div>
       )}
       {modal()}
